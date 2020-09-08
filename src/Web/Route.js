4b82b016116193311eb9867c522a293ea5de1853
Lotinex <loard969@gmail.com>
2020-09-08 23:02:28 +0900

    Language update (1)

--- src/Web/Route.js
+++ src/Web/Route.js
@@ -1,6 +1,7 @@
 const Express = require("express");
 const Login = require("./Login");
 const DB = require("../DB/Database");
+const TABLE = require("./json/lang-ko.json");
 
 module.exports = function(Logger){
     const Router = Express.Router();
@@ -12,7 +13,10 @@ module.exports = function(Logger){
     Router.get("/", (req, res) => {
         res.render("index"); //이제 여기에서 사용자 정보를 전달하도록 한다.
     })
-
+    Router.get("/setupLangTable", (req, res) => {
+        res.send(`window.LANGUAGE_TABLE = ${JSON.stringify(TABLE)};`);
+    })
+    
     return Router;
 }
 
--- src/Web/public/scripts/lib/Language.js
+++ src/Web/public/scripts/lib/Language.js
@@ -4,21 +4,21 @@ const TABLE = window.LANGUAGE_TABLE || {};
  * Implement Me
  */
 class L {
-    static render(key, data){
+    static process(key, data){
         const base = TABLE[key];
         if(base === undefined) return `$${key}$`; // 표에서 적절한 문자열을 찾지 못했을 때
 
+        return base; // 우선 여기까지
 
 
 
     }
     /**
-     * L.render를 overload하기에는 부적합해서 object Expression을 사용하는 함수를 따로 사용
+     * L.process를 overload하기에는 부적합해서 object Expression을 사용하는 함수를 따로 사용
      */
-    static renderResolveObject(key, expression){
+    static processResolveObject(key, expression){
         /**
          * Implement Me
          */
     }
 }
-module.exports = L;
