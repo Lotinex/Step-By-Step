9a886c8bdcdc94d0f197eea6f2e3762f88daf19f
Lotinex <loard969@gmail.com>
2020-09-10 09:15:41 +0900

    Language update

--- src/Tools/Language.js
+++ src/Tools/Language.js
@@ -4,21 +4,27 @@ const TABLE = window.LANGUAGE_TABLE || {};
  * Implement Me
  */
 class L {
-    static render(key, data){
-        const base = TABLE[key];
+    static SYNTAX = {
+        normal: index => `{#${index}}`,
+        FA: () => `{*(\w)}`,
+    }
+    static process(key, ...data){
+        let base = TABLE[key];
         if(base === undefined) return `$${key}$`; // 표에서 적절한 문자열을 찾지 못했을 때
 
 
-
-
+        for(let i in data){
+            base = base.replace(new RegExp(L.SYNTAX.normal(i)), data[i]);
+            base = base.replace(new RegExp(L.SYNTAX.FA()), `<i class="fas fa-$1"></i>`);
+        }
+        return base;
     }
     /**
-     * L.render를 overload하기에는 부적합해서 object Expression을 사용하는 함수를 따로 사용
+     * L.process를 overload하기에는 부적합해서 object Expression을 사용하는 함수를 따로 사용
      */
-    static renderResolveObject(key, expression){
+    static processResolveObject(key, expression){
         /**
          * Implement Me
          */
     }
 }
-module.exports = L;
